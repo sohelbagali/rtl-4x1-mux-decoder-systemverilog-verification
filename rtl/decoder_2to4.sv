21c6b2d9d2afa8d978a9bd72bf306eae933594a0
Sohel Bagali <sohelbagali123@gmail.com>
2026-03-03 19:50:07 +0530

    Create decoder_2to4.sv

---
+++ rtl/decoder_2to4.sv
@@ -0,0 +1,16 @@
+module decoder_2to4 (
+    input  logic [1:0] sel,
+    output logic [3:0] out
+);
+
+    always_comb begin
+        out = 4'b0000;
+        case (sel)
+            2'b00: out = 4'b0001;
+            2'b01: out = 4'b0010;
+            2'b10: out = 4'b0100;
+            2'b11: out = 4'b1000;
+        endcase
+    end
+
+endmodule
